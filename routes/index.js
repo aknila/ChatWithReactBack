ab06ae392bf0275a4dc30bb3139a939f4fb35733
anthony ancel <aancel@MacBook-Air-de-anthony.local>
2019-05-27 13:30:29 +0200

    add file upload

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 const express = require("express");
+var multer = require('multer');
 const router = express.Router();
+
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'public')
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + '-' +file.originalname )
+	}
+})
+
+var upload = multer({ storage: storage }).single('file')
 
 router.get("/", (req, res) => {
   res.send({ response: "I am alive" }).status(200);
 });
+
+router.post("/upload", (req, res) => {
+	console.log('allo');
+	upload(req, res, function (err) {
+		if (err instanceof multer.MulterError) {
+			return res.status(500).json(err)
+		} else if (err) {
+			return res.status(500).json(err)
+		}
+		return res.status(200).send(req.file)
+	})
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,43 +1,87 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-// // const axios = require("axios");
+
+var cors = require('cors');
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 
 const app = express();
+app.use(cors());
 app.use(index);
+app.use('/static', express.static('public'));
 
 const server = http.createServer(app);
 
 const io = socketIo(server);
 
-// const getApiAndEmit = "TODO"
+let socketCount = {
+    '/salon1': 0,
+    '/salon2': 0,
+    '/salon3': 0,
+    '/salon4': 0,
+    '/salon5': 0
+};
 
-let socketCount = 0;
+let msgSave = {
+    '/salon1': [],
+    '/salon2': [],
+    '/salon3': [],
+    '/salon4': [],
+    '/salon5': []
+}
 
-io.on("connection", socket => {
-    socketCount++;
-    console.log("New client connected", socketCount);
-    io.emit('new on', {count: socketCount});
+const saveMsg = (chan, msg) => {
+    msgSave[chan].push(msg);
+}
+
+const editMsg = (chan, msg) => {
+    let edit = msgSave[chan].find(data => data.uid === msg.uid);
+    let ind = msgSave[chan].indexOf(edit);
+    msgSave[chan][ind].text = msg.text;
+}
+
+const delMsg = (chan, msg) => {
+    let copy = msgSave[chan].filter(data => data.uid !== msg.uid)
+    msgSave[chan] = copy;
+}
+
+const watcher = (socket) => {
+    let chan = socket.nsp;
+    socketCount[chan.name] += 1;
+    chan.emit('new on', {count: socketCount[chan.name]});
+    socket.emit('history', {save: msgSave[chan.name].slice(-7)})
     socket.on('chat message', msg => {
         console.log('new message');
-        io.emit('chat message', msg);
+        saveMsg(chan.name, msg);
+        chan.emit('chat message', msg);
     });
     socket.on('delete message', data => {
         console.log('message deleted');
-        io.emit('delete message', data)
+        delMsg(chan.name, data);
+        chan.emit('delete message', data)
     });
     socket.on('edit message', data => {
         console.log('message edited');
-        io.emit('edit message', data)
+        editMsg(chan.name, data);        
+        chan.emit('edit message', data)
     });
     socket.on("disconnect", () => {
         console.log("Client disconnected");
-        socketCount--;
-        io.emit('new off', {count: socketCount});
+        socketCount[chan.name] -= 1;
+        chan.emit('new off', {count: socketCount[chan.name]});
     });
-});
+    socket.on('history', data => {
+        // console.log('history', -data.len);
+        socket.emit('history', {save: msgSave[chan.name].slice(-data.len)})
+    });
+};
+
+const nsp1 = io.of('/salon1').on('connection', watcher)
+const nsp2 = io.of('/salon2').on('connection', watcher)
+const nsp3 = io.of('/salon3').on('connection', watcher)
+const nsp4 = io.of('/salon4').on('connection', watcher)
+const nsp5 = io.of('/salon5').on('connection', watcher)
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
